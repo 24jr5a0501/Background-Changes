f3d65222a9ace40adaa5156de28cc5f85e5e68df
TejaswiniBandaru <24jr5a0501@gmail.com>
2025-03-19 00:34:56 +0530

    script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 console.log("Hello");
-function Display1(){
+function Display1() {
     console.log("Welcome");
 }
 setTimeout(Display1, 3000);
-
-function Display2(){
+function Display2() {
     document.querySelector('h1').innerHTML = 'INTERNSHIP BATCH';
+    console.log("Display2 executed");
 }
-const changeTimeout = setTimeout(Display2, 6000);
+let changeTimeout = setTimeout(Display2, 6000); 
 let changeInterval;
 function randomColor() {
     const hex = '0123456789ABCDEF';
@@ -24,14 +24,17 @@ document.querySelector('#start').addEventListener('click', () => {
     changeInterval = setInterval(changeBgColor, 2000);
     console.log("Background color change started");
 });
-
 document.querySelector('#stop').addEventListener('click', () => {
     if (changeInterval) {
         clearInterval(changeInterval);
         console.log("Stopped background color change");
     }
     if (changeTimeout) {
-        clearTimeout(changeTimeout);
+        clearTimeout(changeTimeout); 
         console.log("Stopped Display2 timeout");
+        changeTimeout = null; 
+    } else {
+        console.log("Restarting Display2 timeout");
+        changeTimeout = setTimeout(Display2, 6000);
     }
 });
